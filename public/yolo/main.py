8390ba1a430b01911c2b2688ccb69728169d6d64
Ngoccham05 <ngoccham0912@gmail.com>
2023-08-25 09:06:48 +0800

    Update Api to save data

--- public/yolo/main.py
+++ public/yolo/main.py
@@ -6,6 +6,11 @@ import torchvision.transforms as T
 import schedule
 import time
 import os
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
 
 model = torch.hub.load('ultralytics/yolov5', 'yolov5m', pretrained=True)
 
@@ -26,7 +31,7 @@ cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 
 count_persons = 0
 image_path = None
-data_sent = False
+start_yolo_detection = 0
 
 while True:
     ret, frame = cap.read()
@@ -44,30 +49,38 @@ while True:
             cv2.putText(frame, classes[class_id], (int(x), int(y)-10), cv2.FONT_HERSHEY_PLAIN, 2, (200, 0, 50), 2)
             cv2.rectangle(frame, (int(x), int(y)), (int(x+w), int(y+h)), (200, 0, 50), 2)
 
-    try:
-        # Đường dẫn thư mục để lưu ảnh
-        image_directory = "D:\_OBJECT-DETECTION\object-detection\public\image_checking"
-        # Lưu ảnh và lấy đường dẫn
-        image_filename = f"checking_{datetime.now().strftime('%Y%m%d%H%M%S')}.jpg"
-        image_path = image_directory + "/" + image_filename
-        cv2.imwrite(image_path, frame)
+    response = requests.get('http://127.0.0.1:5000/send_data_yolo')
 
-        print("Count Persons:", count_persons)
-        response = requests.post('http://127.0.0.1:5000/data_yolo',json={'count_persons': count_persons, 'image_path': image_path})
+    if response.status_code == 200:
+        data_received = response.json()
+        start_yolo_detection = data_received['start_yolo_detection']
 
-        if response.status_code == 200:
-            data = response.json()
-            print('Phản hồi từ máy chủ:', data)
-        else:
-            print('Không thể lấy dữ liệu từ API')
-    except Exception as e:
-        print('Lỗi:', e)
+    if start_yolo_detection == 1:
+        try:
+            # Đường dẫn thư mục để lưu ảnh
+            image_directory = "D:\_OBJECT-DETECTION\object-detection\public\image_checking"
+            # Lưu ảnh và lấy đường dẫn
+            image_filename = f"checking_{datetime.now().strftime('%Y%m%d%H%M%S')}.jpg"
+            image_path = image_directory + "/" + image_filename
+            cv2.imwrite(image_path, frame)
+
+            print("Count Persons:", count_persons)
+            response = requests.post('http://127.0.0.1:5000/data_yolo',json={'count_persons': count_persons, 'image_path': image_path})
+
+            if response.status_code == 200:
+                data = response.json()
+                print('Phản hồi từ máy chủ:', data)
+            else:
+                print('Không thể lấy dữ liệu từ API')
+        except Exception as e:
+            print('Lỗi:', e)
+        start_yolo_detection == 0
 
     cv2.putText(frame, f"Number of persons: {count_persons}", (10, 50), cv2.FONT_HERSHEY_PLAIN, 2, (0, 255, 0), 2)
     cv2.imshow("Frame", frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+        breakz
     count_persons = 0
 
 cap.release()
